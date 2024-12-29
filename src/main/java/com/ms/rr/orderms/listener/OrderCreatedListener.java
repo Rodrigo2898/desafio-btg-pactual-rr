a3f594785369b17a8638c86562ad0beb29713110
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2024-12-29 01:42:51 -0300

    Criando Listener da Fila do RabbitMQ no Spring Boot e consumindo menssagem

---
+++ src/main/java/com/ms/rr/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,21 @@
+package com.ms.rr.orderms.listener;
+
+import com.ms.rr.orderms.listener.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import static com.ms.rr.orderms.config.RabbitMQConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listener(Message<OrderCreatedEvent> message) {
+        logger.info("Message consumed: {}", message);
+    }
+}
