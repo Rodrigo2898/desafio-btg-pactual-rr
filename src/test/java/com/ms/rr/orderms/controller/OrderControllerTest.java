cb62adfc5551fe0584beb4b4d33b4bfa07e6a297
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-01-12 20:22:14 -0300

    Finalizando os testes unitários do OrderController

--- src/test/java/com/ms/rr/orderms/controller/OrderControllerTest.java
+++ src/test/java/com/ms/rr/orderms/controller/OrderControllerTest.java
@@ -5,9 +5,12 @@ import com.ms.rr.orderms.service.OrderService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatusCode;
 
 import java.math.BigDecimal;
@@ -25,6 +28,12 @@ class OrderControllerTest {
 
     @InjectMocks
     OrderController orderController;
+
+    @Captor
+    ArgumentCaptor<Long> customerIdCaptor;
+
+    @Captor
+    ArgumentCaptor<PageRequest> pageRequestCaptor;
 
     @Nested // indicando que essa sub-classe também vai conter teste unitários
     class ListOrders {
@@ -51,10 +60,58 @@ class OrderControllerTest {
 
         @Test
         void shouldPassCorrectParametersToService() {
+            // ARRANGE - prepara todos os mocks para a execução
+            var customerId = 1L;
+            var page = 0;
+            var pageSize = 10;
+
+            var pageResponse = OrderResponseFactory.buildWithOneItem();
+            doReturn(pageResponse).when(orderService).findAllByCustomerId(customerIdCaptor.capture(), pageRequestCaptor.capture());
+
+            BigDecimal totalOnOrders = BigDecimal.valueOf(20.50);
+            doReturn(totalOnOrders).when(orderService).findTotalOnOrdersByCustomerId(customerIdCaptor.capture());
+
+            // ACT - executar o método a ser testado
+            var response = orderController.listOrders(customerId, page, pageSize);
+
+            // ASSERT - verifica se a execução foi correta
+            assertEquals(2, customerIdCaptor.getAllValues().size());
+            assertEquals(customerId, customerIdCaptor.getAllValues().get(0));
+            assertEquals(customerId, customerIdCaptor.getAllValues().get(1));
+            assertEquals(page, pageRequestCaptor.getValue().getPageNumber());
+            assertEquals(pageSize, pageRequestCaptor.getValue().getPageSize());
         }
 
         @Test
         void shouldReturnResponseBodyCorrectly() {
+            // ARRANGE - prepara todos os mocks para a execução
+            var customerId = 1L;
+            var page = 0;
+            var pageSize = 10;
+            var pagination = OrderResponseFactory.buildWithOneItem();
+            BigDecimal totalOnOrders = BigDecimal.valueOf(20.50);
+
+            doReturn(pagination).when(orderService).findAllByCustomerId(anyLong(), any());
+            doReturn(totalOnOrders).when(orderService).findTotalOnOrdersByCustomerId(any());
+
+            // ACT - executar o método a ser testado
+            var response = orderController.listOrders(customerId, page, pageSize);
+
+            // ASSERT - verifica se a execução foi correta
+            assertNotNull(response);
+            assertNotNull(response.getBody());
+            assertNotNull(response.getBody().data());
+            assertNotNull(response.getBody().pagination());
+            assertNotNull(response.getBody().summary());
+
+            assertEquals(totalOnOrders, response.getBody().summary().get("totalOnOrders"));
+
+            assertEquals(pagination.getTotalElements(), response.getBody().pagination().totalElements());
+            assertEquals(pagination.getTotalPages(), response.getBody().pagination().totalPages());
+            assertEquals(pagination.getNumber(), response.getBody().pagination().page());
+            assertEquals(pagination.getSize(), response.getBody().pagination().pageSize());
+
+            assertEquals(pagination.getContent(), response.getBody().data());
         }
     }
 
